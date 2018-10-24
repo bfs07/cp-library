b8e71c142af0ca3ccbbc6cd467fa9123844e1f86
Convidado <bfs.07@hotmail.com>
2018-10-24 13:33:24 -0300

    Added template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 1ll << 62
+#define pb push_back
+#define ii pair<int,int>
+#define OK cerr <<"OK"<< endl
+#define debug(x) cerr << #x " = " << (x) << endl
+#define ff first
+#define ss second
+#define int long long
+
+signed main () {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+}
