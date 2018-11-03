ef10819f44e09ba137d86b409f9f2b212d616788
bfs07 <bfs.07@hotmail.com>
2018-11-02 21:02:15 -0300

    Commented parent in Dijkstra

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -2,9 +2,9 @@ int Dijkstra(int src, int dest, int n) {
 
   priority_queue<ii,vector <ii>,greater<ii>> pq; 
   vector<int> dist(n+1, INF); 
-  vector<vector<int> > parent(n+1);
-  for(int i = 0; i <= n; i++) 
-    parent[i].pb(i);
+  // vector<vector<int> > parent(n+1);
+  // for(int i = 0; i <= n; i++) 
+  //   parent[i].pb(i);
   pq.push(make_pair(0, src));
   dist[src] = 0;
 
@@ -17,13 +17,14 @@ int Dijkstra(int src, int dest, int n) {
       int w = x.ss;
  
       if (dist[u] + w < dist[v]) {
-        parent[v].clear();
-        parent[v].pb(u);
+        // parent[v].clear();
+        // parent[v].pb(u);
         dist[v] = dist[u] + w;
         pq.push(ii(dist[v], v));
-      } else if(dist[u] + w == dist[v]) {
-        parent[v].pb(u);
-      }
+      } 
+      // else if(dist[u] + w == dist[v]) {
+      //   parent[v].pb(u);
+      // }
     }
   } 
   
