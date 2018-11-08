360f1a2980276c548e7a2dd0779fb3bcdbd92f2a
bfs07 <bfs.07@hotmail.com>
2018-11-08 11:26:56 -0300

    Added functions in LCA

--- codes/Graphs/LCA - Shortest Path.cpp
+++ codes/Graphs/LCA - Shortest Path.cpp
@@ -17,72 +17,84 @@ void dfs(int u, int p, int d = 1) {
 	}
 }
 
-void build(int n) {
+void init(int n) {
 
 	for(int i = 0; i <= n; i++) {
 		for(int j = 0; (1<<j) < n; j++) {
 			anc[i][j] = -1;
 		}
 	}
+  
+}
 
-	// ler a aresta i -> x com peso c
-	for(int i = 1; i < n; i++) {
-		int x,c;
-		cin >> x >> c;		
-		adj[x].pb(ii(i,c));
-		adj[i].pb(ii(x,c));
-		anc[i][0] = x;
-	}
+void read(int n) {
+  
+  // ler a aresta i -> x com peso c
+  for(int i = 1; i < n; i++) {
+    int x,c;
+    cin >> x >> c;    
+    adj[x].pb(ii(i,c));
+    adj[i].pb(ii(x,c));
+    anc[i][0] = x;
+  }
+}
 
+void build(int n) {
+  
+  dist[0] = 0; // short path
+  dfs(0,-1);
 
-	dist[0] = 0; // short path
-	dfs(0,-1);
-
-	for(int j = 1; (1<<j) < n; j++) {
-		for(int i = 0; i < n; i++) {
-			if(anc[i][j-1] != -1)
-				anc[i][j] = anc[anc[i][j-1]][j-1];
-		}
-	}
+  for(int j = 1; (1<<j) < n; j++) {
+    for(int i = 0; i < n; i++) {
+      if(anc[i][j-1] != -1)
+        anc[i][j] = anc[anc[i][j-1]][j-1];
+    }
+  }
 
 }
+
+// Codigo para voltar k posicoes a partir de x 
+int goBack(int x, int k)  {
+
+  for(int i = 0; i < 32; i++) {
+  	if(k & (1<<i))
+  		x = anc[x][i];
+  }
+  return x;
+
+}
+
+
 int lca(int a, int b) {
 
-	int ac = a, bc = b;
-	// a esta mais embaixo na arvore
-	if(level[b] > level[a])
-		swap(a,b);
+  int ac = a, bc = b;
+  // a esta mais embaixo na arvore
+  if(level[b] > level[a])
+    swap(a,b);
 
-	int lg = log2(level[a]);
+  int lg = log2(level[a]);
 
-	// colocando a e b no msm nivel
-	for(int i = lg; i >= 0; i--) {
-		if(level[a]-(1<<i) >= level[b]) {
-			a = anc[a][i];
-		}
-	}
-	// return a; // to find lca
-	if(a == b) 
-		return dist[ac] + dist[bc] - 2*dist[a]; // short path
+  // colocando a e b no msm nivel
+  for(int i = lg; i >= 0; i--) {
+    if(level[a]-(1<<i) >= level[b]) {
+      a = anc[a][i];
+    }
+  }
+  
+  if(a == b) 
+    // return a; // to find lca
+    return dist[ac] + dist[bc] - 2*dist[a]; // short path
 
-	// achando o filho do lca
-	for(int i = lg; i >= 0; i--) {
-		if(anc[a][i] != -1 && anc[a][i] != anc[b][i]) {
-			a = anc[a][i];
-			b = anc[b][i];
-		}
-	}
-	// anc[a][0] é o LCA
-	// return anc[a][0]; retorna o lca
-	//cout << "LCA  = " << anc[a][0] << endl;
-	return dist[ac] + dist[bc] - 2*dist[anc[a][0]];
+  // achando o filho do lca
+  for(int i = lg; i >= 0; i--) {
+    if(anc[a][i] != -1 && anc[a][i] != anc[b][i]) {
+      a = anc[a][i];
+      b = anc[b][i];
+    }
+  }
+  // anc[a][0] é o LCA
+  // return anc[a][0]; retorna o lca
+  //cout << "LCA  = " << anc[a][0] << endl;
+  return dist[ac] + dist[bc] - 2*dist[anc[a][0]];
 
 }
-/*
-Codigo para voltar k posicoes a partir de x 
-for(int i = 0; i < 32; i++) {
-	if(k & (1<<i))
-		x = anc[x][i];
-}
-return x;
-*/
