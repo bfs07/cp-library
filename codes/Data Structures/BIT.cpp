594f8e727b5d27d64368067787c17b4638beb726
Bernardo Salmeron <bfsalmeron@outlook.com>
2019-07-16 23:04:58 -0300

    update bit code

--- codes/Data Structures/BIT.cpp
+++ codes/Data Structures/BIT.cpp
@@ -1,40 +1,84 @@
-// SIZE = N
-int bit[(int)2e5+3];
-int arr[(int)2e5+3];
+class BIT
+{
 
-int low(int i) {
-	return (i & (-i));
-}
+private:
 
-void update(int i, int v, int n) {
-	
-	while(i <= n) {
-		bit[i] += v;
-		i += low(i);
+	vector<int> bit;
+	int n;
+
+private:
+
+	int low(int i)
+	{
+		return (i & (-i));
+	}
+
+public:
+
+	BIT(const vector<int> &arr)
+	{
+		// OBS: BIT IS INDEXED FROM 1
+		// THE USE OF 1-BASED ARRAY IS RECOMMENDED 
+
+		assert(arr.front() == 0);
+		this->bit.resize(arr.size(), 0);
+		this->n = (int)arr.size() - 1;
+
+		this->build(arr);
+	}
+
+	BIT(int n)
+	{
+		this->n = n;
+		this->bit.resize(n + 1, 0); 
+	}
+
+	int build(vector<int> &arr)
+	{
+		// OBS: BIT IS INDEXED FROM 1
+		// THE USE OF 1-BASED ARRAY IS RECOMMENDED
+
+		for(int i = 1; i <= this->n; i++)
+		{
+			this->update(i, arr[i]);
+		}
+	}
+
+	void update(int i, const int delta)
+	{	
+		while(i <= this->n)
+		{
+			this->bit[i] += delta;
+			i += this->low(i);
+		}
+	}
+
+	int query(int i)
+	{
+		int sum = 0;
+		while(i > 0)
+		{
+			sum += this->bit[i];
+			i -= this->low(i);
+		}
+
+		return sum;
 	}
 
 }
 
-int query(int i) {
+// int main()
+// {
 
-	int sum = 0;
-	while(i > 0) {
-		sum += bit[i];
-		i -= low(i);
-	}
+// 	vector<int> input = {1,2,3,4,5,6,7};
 
-	return sum;
+// 	BIT ft(input);
 
-}
-
-int build(int n) {
-
-	// OBS: BIT IS INDEXED FROM 1
-	// RECOMMENDED USE 1-BASED ARRAY
-
-	memset(bit, 0, (n+3)*sizeof(int));
-
-	for(int i = 1; i <= n; i++) {
-		update(i, arr[i], n);
-	}
-}
+// 	assert (1 == ft.getSum(binaryIndexedTree, 0));
+// 	assert (3 == ft.getSum(binaryIndexedTree, 1));
+// 	assert (6 == ft.getSum(binaryIndexedTree, 2));
+// 	assert (10 == ft.getSum(binaryIndexedTree, 3));
+// 	assert (15 == ft.getSum(binaryIndexedTree, 4));
+// 	assert (21 == ft.getSum(binaryIndexedTree, 5));
+// 	assert (28 == ft.getSum(binaryIndexedTree, 6));
+// }
