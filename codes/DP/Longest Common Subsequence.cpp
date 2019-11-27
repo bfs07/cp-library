6a19e2586628a9d8534b578c207ac77130af7595
Bernardo Salmeron <bfsalmeron@outlook.com>
2019-11-26 22:09:15 -0300

    Refactor in LCS code

--- codes/DP/Longest Common Subsequence.cpp
+++ codes/DP/Longest Common Subsequence.cpp
@@ -1,50 +1,32 @@
-void lcs(char *X, char *Y, int m, int n){
-   int L[m+1][n+1]; 
-   /* Following steps build L[m+1][n+1] in bottom up fashion. Note that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-   for (int i=0; i<=m; i++){
-     for (int j=0; j<=n; j++){
-       if (i == 0 || j == 0)
-         L[i][j] = 0;
-       else if (X[i-1] == Y[j-1])
-         L[i][j] = L[i-1][j-1] + 1;
-       else
-         L[i][j]=max(L[i-1][j],L[i][j-1]);
-     }
-   }
-   // Following code is used to print LCS
-   int index = L[m][n];// L[m][n] contem o numero de caracteres
+string lcs(string &s, string &t) {
 
-//Create char array to save the lcs string
-   char lcs[index+1];
-   lcs[index] = '\0'; // Set the terminating character 
-   // Start from the right-most-bottom-most corner and
-   // one by one store characters in lcs[]
-   int i = m, j = n;
-   while (i > 0 && j > 0) {
-      // If current character in X[] and Y are same, then
-      // current character is part of LCS
-      if (X[i-1] == Y[j-1]){
-          lcs[index-1] = X[i-1]; // Put current character in result
-          i--; j--; index--;     // reduce values of i, j and index
-      } 
-// If not same then find the larger of two and go in the direction of larger value
-      else if (L[i-1][j] > L[i][j-1])
-         i--;
+  int n = s.size(), m = t.size();
+
+  s.insert(s.begin(), '#');
+  t.insert(t.begin(), '$');
+
+  vector<vector<int>> mat(n + 1, vector<int>(m + 1, 0));
+
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= m; j++) {
+      if(s[i] == t[j])
+        mat[i][j] = mat[i - 1][j - 1] + 1;
       else
-         j--;
-   } 
-   // Print the lcs
- cout<<"LCSof"<<X<<"and"<<Y<<" is " <<lcs;
+        mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]);
+    }
+  }
+
+  string ans;
+  int i = n, j = m;
+  while(i > 0 && j > 0) {
+    if(s[i] == t[j])
+      ans += s[i], i--, j--;
+    else if(mat[i][j - 1] > mat[i - 1][j])
+      j--;
+    else
+      i--;
+  }
+
+  reverse(ans.begin(), ans.end());
+  return ans;
 }
-int main()
-{
-  char X[] = "AGGTAB";
-  char Y[] = "GXTXAYB";
-
-  int m = strlen(X);
-  int n = strlen(Y);
-  lcs(X, Y, m, n);
-  return 0;
-
-}
-
