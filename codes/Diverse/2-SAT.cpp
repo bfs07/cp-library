a846e19301d5c52ac756088c3f6ecc9e0a29891b
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-01-31 19:51:05 -0300

    Update 2-SAT

--- codes/Diverse/2-SAT.cpp
+++ codes/Diverse/2-SAT.cpp
@@ -6,7 +6,7 @@ class SCC {
   int indexed_from;
   // reversed graph
   vector<vector<int>> trans;
-
+ 
  private:
   void dfs_trans(int u, int id) {
     comp[u] = id;
@@ -16,13 +16,13 @@ class SCC {
       if (comp[v] == -1)
         dfs_trans(v, id);
   }
-
+ 
   void get_transpose(vector<vector<int>>& adj) {
     for (int u = indexed_from; u < this->n + indexed_from; u++)
       for(int v: adj[u])
         trans[v].push_back(u);
   }
-
+ 
   void dfs_fill_order(int u, stack<int> &s, vector<vector<int>>& adj) {
     comp[u] = true;
    
@@ -32,10 +32,10 @@ class SCC {
    
     s.push(u);
   }
-
+ 
   // The main function that finds all SCCs
   void compute_SCC(vector<vector<int>>& adj) {
-
+ 
     stack<int> s; 
     // Fill vertices in stack according to their finishing times
     for(int i = indexed_from; i < this->n + indexed_from; i++)
@@ -57,7 +57,7 @@ class SCC {
         dfs_trans(v, id++);
     }
   }
-
+ 
  public:
   // number of the component of the i-th vertex
   // it's always indexed from 0
@@ -65,61 +65,62 @@ class SCC {
   // the i-th vector contains the vertices that belong to the i-th scc
   // it's always indexed from 0
   vector<vector<int>> scc;
-
+ 
   SCC(int n, int indexed_from, vector<vector<int>>& adj) {
     this->n = n;
     this->indexed_from = indexed_from;
     comp.resize(n + 1);
     trans.resize(n + 1);
     scc.resize(n + 1);
-
+ 
     this->compute_SCC(adj);
   }
 };
-
+ 
 // OBS: INDEXED FROM 0
 class SAT_2 {
-
+ 
  private:
   vector<vector<int>> adj;
   int n;
-
+ 
  public:
   vector<bool> ans;
-
+ 
   SAT_2(int n) {
     this->n = n;
     adj.resize(2*n);
     ans.resize(n);
   }
-
+ 
   //  (X v Y)  = (X -> ¬Y) ∧ (¬X -> Y)
   void add_or(int x, bool pos_x, int y, bool pos_y) {
-    assert(0 <= x < n);
-    assert(0 <= y < n);
+    assert(0 <= x); assert(x < n);
+    assert(0 <= y); assert(x < n);
     adj[(x << 1) ^ pos_x].pb((y << 1) ^ (pos_y ^ 1));
     adj[(y << 1) ^ pos_y].pb((x << 1) ^ (pos_x ^ 1));
   }
-
+ 
   // (X xor Y) = (X v Y) ∧ (¬X v ¬Y) 
   // for this function the result is always 0 1 or 1 0 
   void add_xor(int x, bool pos_x, int y, bool pos_y) {
-    assert(0 <= x < n);
-    assert(0 <= y < n);
+    assert(0 <= x); assert(x < n);
+    assert(0 <= y); assert(x < n);
     add_or(x, y, pos_x, pos_y);
     add_or(x, y, pos_x^1, pos_y^1);
   }
-
+ 
   bool check(int n) {
-
+ 
     SCC scc(2*n, 0, adj);
-
+ 
     for(int i = 0; i < n; i++) {
       if(scc.comp[(i << 1) | 1] == scc.comp[(i << 1) | 0])
         return false;
       ans[i] = (scc.comp[(i << 1) | 1] < scc.comp[(i << 1) | 0]);
     }
-
+ 
     return true;
   }
 };
+ 
