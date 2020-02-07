4adb50d652951998a3f94aab93dcd311ea6c2dbc
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-02-07 17:11:47 -0300

    Update suffix array

--- codes/Strings/suffix_array.cpp
+++ codes/Strings/suffix_array.cpp
@@ -17,26 +17,26 @@ void sort(vector<T> &arr, const int max_element, int (*get_key)(T &),
     count[i] += count[i - 1];
 
   for (int i = n - 1; i >= begin; i--) {
-    new_order[count[get_key(arr[i])]] = arr[i];
+    new_order[count[get_key(arr[i])] - (begin == 0)] = arr[i];
     count[get_key(arr[i])]--;
   }
 
-  arr = new_order;
+  arr.swap(new_order);
 }
 
 /// Sorts an array by their pair of ranks stably in ascending order.
 template <typename T> void sort_pairs(vector<T> &arr, const int rank_size) {
   // Sort by the second rank
   RadixSort::sort<T>(
-      arr, rank_size, [](T &item) { return item.first.second; }, 1ll);
+      arr, rank_size, [](T &item) { return item.first.second; }, 0ll);
 
   // Sort by the first rank
   RadixSort::sort<T>(
-      arr, rank_size, [](T &item) { return item.first.first; }, 1ll);
+      arr, rank_size, [](T &item) { return item.first.first; }, 0ll);
 }
 } // namespace RadixSort
 
-/// It is indexed by 1.
+/// It is indexed by 0.
 /// Let the given string be "banana".
 ///
 /// 0 banana                          5 a
@@ -45,7 +45,7 @@ template <typename T> void sort_pairs(vector<T> &arr, const int rank_size) {
 /// 3 ana        alphabetically       0 banana  
 /// 4 na                              4 na   
 /// 5 a                               2 nana
-/// So the suffix array for "banana" is {null, 6, 4, 2, 1, 5, 3}
+/// So the suffix array for "banana" is {5, 3, 1, 0, 4, 2}
 ///
 /// LCP
 ///
@@ -56,7 +56,7 @@ template <typename T> void sort_pairs(vector<T> &arr, const int rank_size) {
 /// 2 na
 /// 0 nana (The last position will always be zero)
 ///
-/// So the LCP for "banana" is {null, 1, 3, 0, 0, 2, 0}
+/// So the LCP for "banana" is {1, 3, 0, 0, 2, 0}
 ///
 class Suffix_Array {
 private:
@@ -64,32 +64,28 @@ private:
   int n;
 
   typedef pair<int, int> Rank;
-  vector<int> suffix_array;
-  vector<int> lcp;
 
 public:
-  Suffix_Array(const string &s) {
+  Suffix_Array(string &s) {
     this->n = s.size();
-    this->s = "#" + s;
+    this->s.swap(s);
+    // this->s = s;
 
     build_suffix_array();
-    lcp = build_lcp();
+    build_lcp();
   }
 
 private:
-  vector<int> build_ranks(const vector<pair<Rank, int>> &ranks) {
-    vector<int> arr(this->n + 1);
-
-    arr[ranks[1].second] = 1;
-    for (int i = 2; i <= n; i++) {
+  /// The vector containing the ranks will be present at ret
+  void build_ranks(const vector<pair<Rank, int>> &ranks, vector<int> &ret) {
+    ret[ranks[0].second] = 1;
+    for (int i = 1; i < n; i++) {
       // If their rank are equal, than its position should be the same.
       if (ranks[i - 1].first == ranks[i].first)
-        arr[ranks[i].second] = arr[ranks[i - 1].second];
+        ret[ranks[i].second] = ret[ranks[i - 1].second];
       else
-        arr[ranks[i].second] = arr[ranks[i - 1].second] + 1;
+        ret[ranks[i].second] = ret[ranks[i - 1].second] + 1;
     }
-
-    return arr;
   }
 
   /// Builds the Suffix Array for the string s.
@@ -98,41 +94,38 @@ private:
   /// Space Complexity: O(n)
   void build_suffix_array() {
     // This tuple below represents the rank and the index associated with it.
-    vector<pair<Rank, int>> ranks(this->n + 1);
-    vector<int> arr;
+    vector<pair<Rank, int>> ranks(this->n);
+    vector<int> arr(this->n);
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
       ranks[i] = pair<Rank, int>(Rank(s[i], 0), i);
 
     RadixSort::sort_pairs(ranks, 256);
-    arr = build_ranks(ranks);
+    build_ranks(ranks, arr);
 
     {
       int jump = 1;
       int max_rank = arr[ranks.back().second];
-
       // It will be compared intervals a pair of intervals (i, jump-1), (i +
       // jump, i + 2*jump - 1). The variable jump is always a power of 2.
       while (max_rank != this->n) {
-        for (int i = 1; i <= this->n; i++) {
+        for (int i = 0; i < this->n; i++) {
           ranks[i].first.first = arr[i];
-          ranks[i].first.second = (i + jump <= this->n ? arr[i + jump] : 0);
+          ranks[i].first.second = (i + jump < this->n ? arr[i + jump] : 0);
           ranks[i].second = i;
         }
 
         RadixSort::sort_pairs(ranks, n);
-
-        arr = build_ranks(ranks);
+        build_ranks(ranks, arr);
 
         max_rank = arr[ranks.back().second];
         jump *= 2;
       }
     }
 
-    this->suffix_array.resize(this->n + 1);
-    this->suffix_array[0] = numeric_limits<int>::min();
-    for (int i = 1; i <= this->n; i++)
-      this->suffix_array[arr[i]] = i;
+    this->sa.resize(this->n);
+    for (int i = 0; i < this->n; i++)
+      this->sa[arr[i] - 1] = i;
   }
 
   /// Builds the lcp (Longest Common Prefix) array for the string s.
@@ -141,24 +134,24 @@ private:
   ///
   /// Time Complexity: O(n)
   /// Space Complexity: O(n)
-  vector<int> build_lcp() {
-    vector<int> lcp(this->n + 1, 0);
-    vector<int> inverse_suffix(this->n + 1, 0);
+  void build_lcp() {
+    lcp.resize(n, 0);
+    vector<int> inverse_suffix(this->n);
 
-    for (int i = 1; i <= n; i++)
-      inverse_suffix[suffix_array[i]] = i;
+    for (int i = 0; i < this->n; i++)
+      inverse_suffix[sa[i]] = i;
 
     int k = 0;
 
-    for (int i = 1; i <= n; i++) {
-      if (inverse_suffix[i] == n) {
+    for (int i = 0; i < this->n; i++) {
+      if (inverse_suffix[i] == this->n - 1) {
         k = 0;
         continue;
       }
 
-      int j = suffix_array[inverse_suffix[i] + 1];
+      int j = sa[inverse_suffix[i] + 1];
 
-      while (i + k <= this->n && j + k <= this->n && s[i + k] == s[j + k])
+      while (i + k < this->n && j + k < this->n && s[i + k] == s[j + k])
         k++;
 
       lcp[inverse_suffix[i]] = k;
@@ -166,15 +159,12 @@ private:
       if (k > 0)
         k--;
     }
-
-    this->lcp[0] = numeric_limits<int>::min();
-    return lcp;
   }
 
 public:
-  const vector<int> &get_suffix_array() { return suffix_array; }
 
-  const vector<int> &get_lcp() { return lcp; }
+  vector<int> sa;
+  vector<int> lcp;
 
   /// LCS of two strings A and B.
   ///
@@ -188,14 +178,13 @@ public:
   /// Time Complexity: O(n)
   /// Space Complexity: O(1)
   int lcs(int separator) {
-    separator++;
     assert(!isalpha(this->s[separator] && !isdigit(this->s[separator])));
 
     int ans = 0;
 
-    for (int i = 1; i < this->n - 1; i++) {
-      int left = this->suffix_array[i];
-      int right = this->suffix_array[i + 1];
+    for (int i = 0; i + 1 < this->sa.size(); i++) {
+      int left = this->sa[i];
+      int right = this->sa[i + 1];
 
       if ((left < separator && right > separator) ||
           (left > separator && right < separator))
