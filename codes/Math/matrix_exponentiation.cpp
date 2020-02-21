2bb80c05e8c164f7b9a7c880629443105c112be0
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-02-21 18:47:02 -0300

    Create Matrix Exponentiation

---
+++ codes/Math/matrix_exponentiation.cpp
@@ -0,0 +1,52 @@
+namespace matrix {
+
+  using Matrix = vector<vector<int>>;
+
+  /// Creates an n x n identity matrix.
+  ///
+  /// Time Complexity: O(n*n)
+  Matrix identity(const int n) {
+    Matrix mat_identity(n, vector<int>(n, 0));
+
+    for(int i = 0; i < n; i++)
+      mat_identity[i][i] = 1;
+
+    return mat_identity;
+  }
+
+  /// Multiplies matrices a and b.
+  ///
+  /// Time Complexity: O(mat.size() ^ 3)
+  Matrix mult(const Matrix &a, const Matrix &b, const int MOD) {
+    assert(a.front().size() == b.size());
+    
+    Matrix ans(a.size(), vector<int>(b.front().size(), 0));
+    for(int i = 0; i < ans.size(); i++)
+      for(int j = 0; j < ans.front().size(); j++)
+        for(int k = 0; k < a.front().size(); k++)
+          ans[i][j] = (ans[i][j] + a[i][k] * b[k][j]) % MOD;
+
+    return ans;
+  }
+
+  /// Exponentiates the matrix mat to the power of p.
+  ///
+  /// Time Complexity: O((mat.size() ^ 3) * log2(p))
+  Matrix expo(Matrix &mat, int p, const int MOD) {
+
+    Matrix ans = identity(mat.size());
+    Matrix cur_power;
+    cur_power.swap(mat);
+
+    while(p) {
+      if(p & 1)
+        ans = mult(ans, cur_power, MOD);
+
+      cur_power = mult(cur_power, cur_power, MOD);
+      p >>= 1;
+    }
+
+    return ans;
+  }
+
+};
