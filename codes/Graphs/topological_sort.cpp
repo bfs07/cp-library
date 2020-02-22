bd892be818d9adf431c5a7e33ad7959f70e2706a
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-02-21 22:22:17 -0300

    Update Topological Sort

---
+++ codes/Graphs/topological_sort.cpp
@@ -0,0 +1,36 @@
+/// INDEXED BY ZERO 
+///  
+/// Time Complexity: O(n) 
+vector<int> topological_sort(int n) {
+  vector<int> in_degree(n, 0);
+
+  for(int u = 0; u < n; u++)
+    for(int v: adj[u])
+      in_degree[v]++;
+
+  queue<int> q;
+  for(int i = 0; i < n; i++)
+    if(in_degree[i] == 0) 
+      q.push(i);
+ 
+  int cnt = 0;
+  vector<int> top_order;
+  while(!q.empty()) {
+    int u = q.front();
+    q.pop();
+    
+    top_order.push_back(u); 
+    cnt++;
+
+    for(int v: adj[u]) {
+      if(--in_degree[v] == 0)
+        q.push(v); 
+  }
+ 
+  if(cnt != n) {
+    cerr << "There exists a cycle in the graph" << endl;
+    return vector<int>();
+  } 
+
+  return top_order;
+}
