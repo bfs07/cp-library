6b21af95177f28d0e417a4c9edc3a5b1215593ac
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-03-03 23:20:41 -0300

    Update HLD

--- codes/Graphs/hld.cpp
+++ codes/Graphs/hld.cpp
@@ -5,19 +5,17 @@ class HLD {
   vector<int> sz;
 
  private:
-  int get_sz(const int u, const int p, const vector<vector<ii>> &adj) {
+  int get_sz(const int u, const int p, const vector<vector<int>> &adj) {
     this->sz[u] = 1;
-    for(const ii &v: adj[u]) {
-      if(v.ff == p)
+    for(const int v: adj[u]) {
+      if(v == p)
         continue;
-      this->sz[u] += this->get_sz(v.ff, u, adj);
+      this->sz[u] += this->get_sz(v, u, adj);
     }
     return this->sz[u];
   }
 
-  void dfs(const int u, const int id, const int p, const int w, const vector<vector<ii>> &adj) {
-
-    this->weight[u] = w;
+  void dfs(const int u, const int id, const int p, const vector<vector<int>> &adj) {
     this->chain_id[u] = id;
     this->id_in_chain[u] = chain_size[id];
     this->parent[u] = p;
@@ -26,42 +24,42 @@ class HLD {
       this->chain_head[id] = u;
     this->chain_size[id]++;
 
-    int maxx = -1, idx = -1, w_idx;
-    for(const ii &v: adj[u]) {
-      if(v.ff == p)
+    int maxx = -1, idx = -1;
+    for(const int v: adj[u]) {
+      if(v == p)
         continue;
-      if(sz[v.ff] > maxx) {
-        maxx = sz[v.ff];
-        idx = v.ff;
-        w_idx = v.ss;
+      if(sz[v] > maxx) {
+        maxx = sz[v];
+        idx = v;
       }
     }
 
     if(idx != -1)
-      this->dfs(idx, id, u, w_idx, adj);
+      this->dfs(idx, id, u, adj);
 
-    for(const ii &v: adj[u]) {
-      if(v.ff == idx || v.ff == p)
+    for(const int v: adj[u]) {
+      if(v == idx || v == p)
         continue;
-      this->dfs(v.ff, this->number_of_chains++, u, v.ss, adj);
+      this->dfs(v, this->number_of_chains++, u, adj);
     }
   }
 
  public:
-  HLD(const vector<vector<ii>> &adj) {
+  /// Builds the paths.
+  ///
+  /// Time Complexity: O(n)
+  HLD(const vector<vector<int>> &adj) {
     this->n = adj.size();
     this->chain_head.resize(this->n + 1, -1);
     this->id_in_chain.resize(this->n + 1, -1);
     this->chain_id.resize(this->n + 1, -1);
     this->sz.resize(this->n + 1);
     this->chain_size.resize(this->n + 1);
-    this->weight.resize(this->n + 1);
-    this->parent.resize(this->n + 1);
+    this->parent.resize(this->n + 1, -1);
     this->get_sz(1, -1, adj);
-    this->dfs(1, 0, -1, -1, adj);
+    this->dfs(1, 0, -1, adj);
   }
 
- public:
   // the chains are indexed from 0
   int number_of_chains = 1;
   // topmost node of the chain
@@ -72,9 +70,6 @@ class HLD {
   vector<int> chain_id;
   // size of the i-th chain
   vector<int> chain_size;
-  // weight of the i-th vertex, the bottommost edge will store
-  // the weight of the edge. The root has weight -1 
-  vector<int> weight;
-  // parent of the i-th node
+  // parent of the i-th node, -1 for root
   vector<int> parent;
 };
