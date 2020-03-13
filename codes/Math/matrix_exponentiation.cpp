58e53aa3e888504f1412e830776ba69d4cb8e493
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-03-13 18:30:48 -0300

    Format and update matrix expo code to speed up MOD operation

--- codes/Math/matrix_exponentiation.cpp
+++ codes/Math/matrix_exponentiation.cpp
@@ -1,52 +1,54 @@
 namespace matrix {
-  #define Matrix vector<vector<int>>
-  /// Creates an n x n identity matrix.
-  ///
-  /// Time Complexity: O(n*n)
-  Matrix identity(const int n) {
-    assert(n > 0);
+#define Matrix vector<vector<int>>
+const int MOD = 1e9 + 7;
 
-    Matrix mat_identity(n, vector<int>(n, 0));
+/// Creates an n x n identity matrix.
+///
+/// Time Complexity: O(n*n)
+Matrix identity(const int n) {
+  assert(n > 0);
 
-    for(int i = 0; i < n; i++)
-      mat_identity[i][i] = 1;
+  Matrix mat_identity(n, vector<int>(n, 0));
 
-    return mat_identity;
+  for (int i = 0; i < n; i++)
+    mat_identity[i][i] = 1;
+
+  return mat_identity;
+}
+
+/// Multiplies matrices a and b.
+///
+/// Time Complexity: O(mat.size() ^ 3)
+Matrix mult(const Matrix &a, const Matrix &b) {
+  assert(a.front().size() == b.size());
+
+  Matrix ans(a.size(), vector<int>(b.front().size(), 0));
+  for (int i = 0; i < ans.size(); i++)
+    for (int j = 0; j < ans.front().size(); j++)
+      for (int k = 0; k < a.front().size(); k++)
+        ans[i][j] = (ans[i][j] + a[i][k] * b[k][j]) % MOD;
+
+  return ans;
+}
+
+/// Exponentiates the matrix mat to the power of p.
+///
+/// Time Complexity: O((mat.size() ^ 3) * log2(p))
+Matrix expo(Matrix &mat, int p) {
+  assert(p >= 0);
+
+  Matrix ans = identity(mat.size());
+  Matrix cur_power;
+  cur_power.swap(mat);
+
+  while (p) {
+    if (p & 1)
+      ans = mult(ans, cur_power);
+
+    cur_power = mult(cur_power, cur_power);
+    p >>= 1;
   }
 
-  /// Multiplies matrices a and b.
-  ///
-  /// Time Complexity: O(mat.size() ^ 3)
-  Matrix mult(const Matrix &a, const Matrix &b, const int MOD) {
-    assert(a.front().size() == b.size());
-    
-    Matrix ans(a.size(), vector<int>(b.front().size(), 0));
-    for(int i = 0; i < ans.size(); i++)
-      for(int j = 0; j < ans.front().size(); j++)
-        for(int k = 0; k < a.front().size(); k++)
-          ans[i][j] = (ans[i][j] + a[i][k] * b[k][j]) % MOD;
-
-    return ans;
-  }
-
-  /// Exponentiates the matrix mat to the power of p.
-  ///
-  /// Time Complexity: O((mat.size() ^ 3) * log2(p))
-  Matrix expo(Matrix &mat, int p, const int MOD) {
-    assert(p >= 0);
-
-    Matrix ans = identity(mat.size());
-    Matrix cur_power;
-    cur_power.swap(mat);
-
-    while(p) {
-      if(p & 1)
-        ans = mult(ans, cur_power, MOD);
-
-      cur_power = mult(cur_power, cur_power, MOD);
-      p >>= 1;
-    }
-
-    return ans;
-  }
-};
+  return ans;
+}
+}; // namespace matrix
