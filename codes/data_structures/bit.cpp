1a845a652b8091146d27fe34a02c54795332f3c6
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-03-18 12:13:04 -0300

    Update and rename bit codes

--- codes/data_structures/bit.cpp
+++ codes/data_structures/bit.cpp
@@ -1,17 +1,15 @@
 /// INDEX THE ARRAY BY 1!!!
 class BIT {
- private:
+private:
   vector<int> bit;
   int n;
 
- private:
-  int low(const int i) {
-    return (i & (-i));
-  }
+private:
+  int low(const int i) { return (i & (-i)); }
 
   // point update
-  void bit_update(int i, const int delta) { 
-    while(i <= this->n) {
+  void bit_update(int i, const int delta) {
+    while (i <= this->n) {
       this->bit[i] += delta;
       i += this->low(i);
     }
@@ -20,23 +18,21 @@ class BIT {
   // point query
   int bit_query(int i) {
     int sum = 0;
-    while(i > 0) {
+    while (i > 0) {
       sum += bit[i];
       i -= this->low(i);
     }
     return sum;
   }
 
- public:
-  BIT(const vector<int> &arr) {
-    this->build(arr);
-  }
+public:
+  BIT(const vector<int> &arr) { this->build(arr); }
 
   BIT(const int n) {
     // OBS: BIT IS INDEXED FROM 1
-    // THE USE OF 1-BASED ARRAY IS RECOMMENDED 
-    this->n = n; 
-    this->bit.resize(n + 1, 0); 
+    // THE USE OF 1-BASED ARRAY IS RECOMMENDED
+    this->n = n;
+    this->bit.resize(n + 1, 0);
   }
 
   // build the bit
@@ -47,19 +43,25 @@ class BIT {
     this->n = (int)arr.size() - 1;
     this->bit.resize(arr.size(), 0);
 
-    for(int i = 1; i <= this->n; i++)
+    for (int i = 1; i <= this->n; i++)
       this->bit_update(i, arr[i]);
   }
 
   // point update
   void update(const int i, const int delta) {
-    assert(1 <= i); assert(i <= this->n);
-    this->bit_update(i, delta); 
+    assert(1 <= i), assert(i <= this->n);
+    this->bit_update(i, delta);
   }
 
   // point query
   int query(const int i) {
-    assert(1 <= i); assert(i <= this->n);
+    assert(1 <= i), assert(i <= this->n);
     return this->bit_query(i);
+  }
+
+  // range query
+  int query(const int l, const int r) {
+    assert(1 <= l), assert(l <= r), assert(r <= this->n);
+    return this->bit_query(r) - this->bit_query(l - 1);
   }
 };
