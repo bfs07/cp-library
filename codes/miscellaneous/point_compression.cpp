0ab4d1c8878123828217b6c6ca6d55ad29a30d38
Bernardo Salmeron <bfsalmeron@outlook.com>
2020-03-18 11:46:11 -0300

    Update and rename point compression code

---
+++ codes/miscellaneous/point_compression.cpp
@@ -0,0 +1,17 @@
+// map<int, int> rev;
+
+/// Compress points in the array arr to the range [0..n-1].
+///
+/// Time Complexity: O(n log n)
+vector<int> compress(vector<int> &arr) {
+  vector<int> aux = arr;
+  sort(aux.begin(), aux.end());
+  aux.erase(unique(aux.begin(), aux.end()), aux.end());
+
+  for (size_t i = 0; i < arr.size(); i++) {
+    int id = lower_bound(aux.begin(), aux.end(), arr[i]) - aux.begin();
+    // rev[id] = arr[i];
+    arr[i] = id;
+  }
+  return arr;
+}
