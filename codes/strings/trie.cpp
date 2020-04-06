774a5796d4943b641c47396b204b7488b0e0c17a
bfs07 <bfs.07@hotmail.com>
2020-04-05 21:54:24 -0300

    Update trie

--- codes/strings/trie.cpp
+++ codes/strings/trie.cpp
@@ -4,64 +4,69 @@ private:
   // static const int INT_LEN = 63;
 
   struct Node {
-    map<char, Node *> trans;
-    bool word = false;
-    // cont counts number of words which pass in that node
-    int cont = 0;
+    map<char, Node *> next;
+    // cnt counts the number of words which pass in that node
+    int cnt = 0;
+    // word counts the number of words ending at that node
+    int word_cnt = 0;
   };
   Node *trie_head;
   int trie_size = 0;
 
 private:
-  Node *make_node() { return (new Node()); }
+  Node *make_node() { return new Node(); }
 
   void trie_insert(const string &s) {
     Node *aux = this->head();
-    for (char c : s) {
-      if (!(aux->trans).count(c))
-        aux->trans[c] = make_node();
-      aux = aux->trans[c];
-      ++(aux->cont);
+    for (const char c : s) {
+      if (!(aux->next.count(c)))
+        aux->next[c] = make_node();
+      aux = aux->next[c];
+      ++(aux->cnt);
     }
-    aux->word = true;
+    ++aux->word_cnt;
     ++this->trie_size;
   }
 
   void trie_erase(const string &s) {
     Node *aux = this->head();
-    for (char c : s) {
+    for (const char c : s) {
       Node *last = aux;
-      aux = aux->trans[c];
-      if (--(aux->cont) == 0) {
-        last->trans.erase(c);
+      aux = aux->next[c];
+      --aux->cnt;
+      if (aux->cnt == 0) {
+        last->next.erase(c);
+        aux = nullptr;
         break;
       }
     }
+    if (aux != nullptr)
+      --aux->word_cnt;
     --this->trie_size;
   }
 
-  int trie_query(const string &s) {
+  int trie_count(const string &s) {
     Node *aux = this->head();
-    for (char c : s) {
-      if ((aux->trans).count(c))
-        aux = aux->trans[c];
+    for (const char c : s) {
+      if (aux->next.count(c))
+        aux = aux->next[c];
       else
         return 0;
     }
-    return (*aux).cont;
+    return aux->word_cnt;
   }
 
   int trie_query_xor_max(const string &s) {
     Node *aux = this->head();
     int ans = 0;
-    for (char c : s) {
-      char inv = (c == '0' ? '1' : '0');
-      if ((aux->trans).count(inv)) {
+    for (const char c : s) {
+      const char inv = (c == '0' ? '1' : '0');
+      if (aux->next.count(inv)) {
         ans = ((ans << 1ll) | (inv - '0'));
-        aux = aux->trans[inv];
-      } else if ((aux->trans).count(c)) {
+        aux = aux->next[inv];
+      } else {
         ans = ((ans << 1ll) | (c - '0'));
-        aux = aux->trans[c];
+        aux = aux->next[c];
       }
     }
     return ans;
@@ -88,8 +93,7 @@ public:
   void insert(const int x) {
     assert(x >= 0);
     // converting x to binary representation
-    const string binary_rep = bitset<INT_LEN>(x).to_string();
-    this->trie_insert(binary_rep);
+    this->trie_insert(bitset<INT_LEN>(x).to_string());
   }
 
   /// Removes the string s from the trie.
@@ -103,8 +107,7 @@ public:
   void erase(const int x) {
     assert(x >= 0);
     // converting x to binary representation
-    const string binary_rep = bitset<INT_LEN>(x).to_string();
-    this->trie_erase(binary_rep);
+    this->trie_erase(bitset<INT_LEN>(x).to_string());
   }
 
   /// Returns the number of maximum xor sum with x present in the trie.
@@ -113,12 +116,11 @@ public:
   int query_xor_max(const int x) {
     assert(x >= 0);
     // converting x to binary representation
-    const string binary_rep = bitset<INT_LEN>(x).to_string();
-    return this->trie_query_xor_max(binary_rep);
+    return this->trie_query_xor_max(bitset<INT_LEN>(x).to_string());
   }
 
   /// Returns the number of strings equal to s present in the trie.
   ///
   /// Time Complexity: O(log n)
-  int query(const string &s) { return this->trie_query(s); }
+  int count(const string &s) { return this->trie_count(s); }
 };
