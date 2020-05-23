545071da7105e0b62773be4ca96ccbd39767a781
bfs07 <bfsalmeron@outlook.com>
2020-05-23 19:44:47 -0300

    Update HLD

--- codes/graphs/hld.cpp
+++ codes/graphs/hld.cpp
@@ -1,54 +1,55 @@
 class HLD {
- private:
+private:
   int n;
   // number of nodes below the i-th node
   vector<int> sz;
 
- private:
+private:
   int get_sz(const int u, const int p, const vector<vector<int>> &adj) {
     this->sz[u] = 1;
-    for(const int v: adj[u]) {
-      if(v == p)
+    for (const int v : adj[u]) {
+      if (v == p)
         continue;
       this->sz[u] += this->get_sz(v, u, adj);
     }
     return this->sz[u];
   }
 
-  void dfs(const int u, const int id, const int p, const vector<vector<int>> &adj) {
+  void dfs(const int u, const int id, const int p,
+           const vector<vector<int>> &adj) {
     this->chain_id[u] = id;
     this->id_in_chain[u] = chain_size[id];
     this->parent[u] = p;
 
-    if(this->chain_head[id] == -1)
+    if (this->chain_head[id] == -1)
       this->chain_head[id] = u;
     this->chain_size[id]++;
 
     int maxx = -1, idx = -1;
-    for(const int v: adj[u]) {
-      if(v == p)
+    for (const int v : adj[u]) {
+      if (v == p)
         continue;
-      if(sz[v] > maxx) {
+      if (sz[v] > maxx) {
         maxx = sz[v];
         idx = v;
       }
     }
 
-    if(idx != -1)
+    if (idx != -1)
       this->dfs(idx, id, u, adj);
 
-    for(const int v: adj[u]) {
-      if(v == idx || v == p)
+    for (const int v : adj[u]) {
+      if (v == idx || v == p)
         continue;
       this->dfs(v, this->number_of_chains++, u, adj);
     }
   }
 
- public:
-  /// Builds the paths.
+public:
+  /// Builds the chains.
   ///
   /// Time Complexity: O(n)
-  HLD(const vector<vector<int>> &adj) {
+  HLD(const int root_idx, const vector<vector<int>> &adj) {
     this->n = adj.size();
     this->chain_head.resize(this->n + 1, -1);
     this->id_in_chain.resize(this->n + 1, -1);
@@ -56,8 +57,8 @@ class HLD {
     this->sz.resize(this->n + 1);
     this->chain_size.resize(this->n + 1);
     this->parent.resize(this->n + 1, -1);
-    this->get_sz(1, -1, adj);
-    this->dfs(1, 0, -1, adj);
+    this->get_sz(root_idx, -1, adj);
+    this->dfs(root_idx, 0, -1, adj);
   }
 
   // the chains are indexed from 0
