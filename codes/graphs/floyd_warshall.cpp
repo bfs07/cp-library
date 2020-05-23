6834456357be894e536dd383a39af0d7000a77f7
bfs07 <bfsalmeron@outlook.com>
2020-05-23 19:43:42 -0300

    Update floyd warshall

--- codes/graphs/floyd_warshall.cpp
+++ codes/graphs/floyd_warshall.cpp
@@ -1,11 +1,11 @@
-// OBS: ZERAR adj[i][i] sempre
-for(int i = 0; i < n; i++)
-  adj[i][i] = 0;
+/// Put n = n + 1 for 1 based.
+void floyd_warshall(const int n) {
+  // OBS: Always assign adj[i][i] = 0.
+  for (int i = 0; i < n; i++)
+    adj[i][i] = 0;
 
-for(int k = 0; k < n; k++) {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < n; j++) {
-      adj[i][j] = min(adj[i][j],adj[i][k] + adj[k][j]);
-    }
-  }
+  for (int k = 0; k < n; k++)
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
 }
