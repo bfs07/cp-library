c95ffec941d873a7eb7eb4b05d376e8ff11ce3fe
bfs07 <bfsalmeron@outlook.com>
2020-05-27 12:36:09 -0300

    Remove unuseful files

--- codes/language_stuff/Printf de uma string.cpp
+++
@@ -1,6 +0,0 @@
-char buffer [50];
-int n, a=5, b=3;
-n=sprintf (buffer, "%d plus %d is %d", a, b, a+b);
-printf ("[%s] is a string %d chars long\n",buffer,n);
-// Output:
-// [5 plus 3 is 8] is a string 13 chars long
