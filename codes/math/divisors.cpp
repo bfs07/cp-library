4c2f672bab2cf9aa2b6fbb7dcb5b55d0802e4ae2
bfs07 <bfsalmeron@outlook.com>
2020-06-06 15:15:46 -0300

    Update and format divisors code

--- codes/math/divisors.cpp
+++ codes/math/divisors.cpp
@@ -1,15 +1,17 @@
 /// OBS: Each number has at most ∛N divisors
 /// THE NUMBERS ARE NOT SORTED!!!
+///
+/// Time Complexity: O(sqrt(n))
 vector<int> divisors(int n) {
   vector<int> ans;
   for (int i = 1; i * i <= n; i++) {
     if (n % i == 0) {
-      // If divisors are equal, print only one
       if (n / i == i)
         ans.emplace_back(i);
-      else // Otherwise print both
+      else
         ans.emplace_back(i), ans.emplace_back(n / i);
     }
   }
+  // sort(ans.begin(), ans.end());
   return ans;
 }
