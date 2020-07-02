7bcc0355453c791c27b5a6be9a18400b9448465f
bfs07 <bfsalmeron@outlook.com>
2020-07-02 11:23:44 -0300

    Update and merge suffix array

--- codes/strings/suffix_array.cpp
+++ codes/strings/suffix_array.cpp
@@ -1,3 +1,6 @@
+// To use the compare method use the macro below.
+// #define BUILD_TABLE
+
 namespace RadixSort {
 /// Sorts the array arr stably in ascending order.
 ///
@@ -5,34 +8,34 @@ namespace RadixSort {
 /// Space Complexity: O(n + max_element)
 template <typename T>
 void sort(vector<T> &arr, const int max_element, int (*get_key)(T &),
-          int begin = 0) {
+          const int begin = 0) {
   const int n = arr.size();
   vector<T> new_order(n);
   vector<int> count(max_element + 1, 0);
 
-  for (int i = begin; i < n; i++)
-    count[get_key(arr[i])]++;
+  for (int i = begin; i < n; ++i)
+    ++count[get_key(arr[i])];
 
-  for (int i = 1; i <= max_element; i++)
+  for (int i = 1; i <= max_element; ++i)
     count[i] += count[i - 1];
 
-  for (int i = n - 1; i >= begin; i--) {
+  for (int i = n - 1; i >= begin; --i) {
     new_order[count[get_key(arr[i])] - (begin == 0)] = arr[i];
-    count[get_key(arr[i])]--;
+    --count[get_key(arr[i])];
   }
 
-  arr.swap(new_order);
+  arr = move(new_order);
 }
 
 /// Sorts an array by their pair of ranks stably in ascending order.
 template <typename T> void sort_pairs(vector<T> &arr, const int rank_size) {
-  // Sort by the second rank
+  // sort by the second rank
   RadixSort::sort<T>(
-      arr, rank_size, [](T &item) { return item.first.second; }, 0ll);
+      arr, rank_size, [](T &item) { return item.first.second; }, 0);
 
-  // Sort by the first rank
+  // sort by the first rank
   RadixSort::sort<T>(
-      arr, rank_size, [](T &item) { return item.first.first; }, 0ll);
+      arr, rank_size, [](T &item) { return item.first.first; }, 0);
 }
 } // namespace RadixSort
 
@@ -60,28 +63,37 @@ template <typename T> void sort_pairs(vector<T> &arr, const int rank_size) {
 ///
 class Suffix_Array {
 private:
-  string s;
-  int n;
+  const string s;
+  const int n;
 
   typedef pair<int, int> Rank;
+#ifdef BUILD_TABLE
+  vector<vector<int>> rank_table;
+  vector<int> log_array;
+#endif
 
 public:
-  Suffix_Array(string &s) {
-    this->n = s.size();
-    this->s = s;
-    // little optimization, remove the line above
-    // this->s.swap(s);
-
+  Suffix_Array(const string &s) : n(s.size()), s(s) {
+#ifdef BUILD_TABLE
+    this->log_array = build_log_array();
+#endif
     this->sa = build_suffix_array();
     this->lcp = build_lcp();
   }
 
 private:
-  /// The vector containing the ranks will be present at ret
+  vector<int> build_log_array() {
+    vector<int> log_array(this->n + 1, 0);
+    for (int i = 2; i <= this->n; ++i)
+      log_array[i] = log_array[i / 2] + 1;
+    return log_array;
+  }
+
   void build_ranks(const vector<pair<Rank, int>> &ranks, vector<int> &ret) {
+    // The vector containing the ranks will be present at ret
     ret[ranks[0].second] = 1;
-    for (int i = 1; i < n; i++) {
-      // If their rank are equal, than its position should be the same.
+    for (int i = 1; i < n; ++i) {
+      // if their rank are equal, than their position should be the same
       if (ranks[i - 1].first == ranks[i].first)
         ret[ranks[i].second] = ret[ranks[i - 1].second];
       else
@@ -92,29 +104,45 @@ private:
   /// Builds the Suffix Array for the string s.
   ///
   /// Time Complexity: O(n*log(n))
-  /// Space Complexity: O(n)
   vector<int> build_suffix_array() {
-    // This tuple below represents the rank and the index associated with it.
+    // the tuple below represents the rank and the index associated with it
     vector<pair<Rank, int>> ranks(this->n);
     vector<int> arr(this->n);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
       ranks[i] = pair<Rank, int>(Rank(s[i], 0), i);
 
+#ifdef BUILD_TABLE
+    int rank_table_size = 0;
+    this->rank_table.resize(log_array[this->n] + 2);
+#endif
     RadixSort::sort_pairs(ranks, 256);
     build_ranks(ranks, arr);
 
     {
       int jump = 1;
       int max_rank = arr[ranks.back().second];
-      // It will be compared intervals a pair of intervals (i, jump-1), (i +
-      // jump, i + 2*jump - 1). The variable jump is always a power of 2.
+
+      // it will be compared intervals a pair of intervals (i, jump-1), (i +
+      // jump, i + 2*jump - 1). The variable jump is always a power of 2
+#ifdef BUILD_TABLE
+      while (jump / 2 < this->n) {
+#else
       while (max_rank != this->n) {
-        for (int i = 0; i < this->n; i++) {
+#endif
+
+        for (int i = 0; i < this->n; ++i) {
           ranks[i].first.first = arr[i];
           ranks[i].first.second = (i + jump < this->n ? arr[i + jump] : 0);
           ranks[i].second = i;
         }
+
+#ifdef BUILD_TABLE
+        // inserting only the ranks in the table
+        transform(ranks.begin(), ranks.end(),
+                  back_inserter(rank_table[rank_table_size++]),
+                  [](pair<Rank, int> &pair) { return pair.first.first; });
+#endif
 
         RadixSort::sort_pairs(ranks, n);
         build_ranks(ranks, arr);
@@ -125,7 +153,7 @@ private:
     }
 
     vector<int> sa(this->n);
-    for (int i = 0; i < this->n; i++)
+    for (int i = 0; i < this->n; ++i)
       sa[arr[i] - 1] = i;
     return sa;
   }
@@ -135,17 +163,16 @@ private:
   /// suffixes indexed by i and i + 1. Implementation of the Kasai's Algorithm.
   ///
   /// Time Complexity: O(n)
-  /// Space Complexity: O(n)
   vector<int> build_lcp() {
-    lcp.resize(n, 0);
+    vector<int> lcp(this->n, 0);
     vector<int> inverse_suffix(this->n);
 
-    for (int i = 0; i < this->n; i++)
+    for (int i = 0; i < this->n; ++i)
       inverse_suffix[sa[i]] = i;
 
     int k = 0;
 
-    for (int i = 0; i < this->n; i++) {
+    for (int i = 0; i < this->n; ++i) {
       if (inverse_suffix[i] == this->n - 1) {
         k = 0;
         continue;
@@ -154,12 +181,12 @@ private:
       int j = sa[inverse_suffix[i] + 1];
 
       while (i + k < this->n && j + k < this->n && s[i + k] == s[j + k])
-        k++;
+        ++k;
 
       lcp[inverse_suffix[i]] = k;
 
       if (k > 0)
-        k--;
+        --k;
     }
 
     return lcp;
@@ -180,14 +207,14 @@ public:
   ///
   /// Time Complexity: O(n)
   /// Space Complexity: O(1)
-  int lcs(int separator) {
+  int lcs(const int separator) {
     assert(!isalpha(this->s[separator] && !isdigit(this->s[separator])));
 
     int ans = 0;
 
-    for (int i = 0; i + 1 < this->sa.size(); i++) {
-      int left = this->sa[i];
-      int right = this->sa[i + 1];
+    for (int i = 0; i + 1 < this->sa.size(); ++i) {
+      const int left = this->sa[i];
+      const int right = this->sa[i + 1];
 
       if ((left < separator && right > separator) ||
           (left > separator && right < separator))
@@ -196,4 +223,27 @@ public:
 
     return ans;
   }
+
+#ifdef BUILD_TABLE
+  /// Compares two substrings beginning at indexes i and j of a fixed length.
+  ///
+  /// Time Complexity: O(1)
+  /// Space Complexity: O(1)
+  int compare(const int i, const int j, const int length) {
+    assert(0 <= i && i < this->n && 0 <= j && j < this->n);
+    assert(i + length - 1 < this->n && j + length - 1 < this->n);
+
+    // Greatest k such that 2^k <= l
+    const int k = this->log_array[length];
+    const int jump = length - (1ll << k);
+
+    const pair<int, int> iRank = {
+        this->rank_table[k][i],
+        (i + jump < this->n ? this->rank_table[k][i + jump] : -1)};
+    const pair<int, int> jRank = {
+        this->rank_table[k][j],
+        (j + jump < this->n ? this->rank_table[k][j + jump] : -1)};
+    return iRank == jRank ? 0 : iRank < jRank ? -1 : 1;
+  }
+#endif
 };
