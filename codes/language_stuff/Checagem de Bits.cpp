77f7ee81fef819c9cf70c332446df81648f68e82
bfs07 <bfsalmeron@outlook.com>
2020-08-01 00:20:40 -0300

    Update counting bits (__builtin_popcount stuff)

--- codes/language_stuff/Checagem de Bits.cpp
+++
@@ -1,5 +0,0 @@
-// OBS: SO FUNCIONA PARA INT (NAO FUNCIONA COM LONG LONG)
-__builtin_popcount(int) -> Número de bits ativos;
-__builtin_ctz(int) -> Número de zeros à direita
-__builtin_clz(int) -> Número de zeros à esquerda
-__builtin_parity(int) -> Retorna se a quantidade de uns é ímpar(1) ou par(0)
