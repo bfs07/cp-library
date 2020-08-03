73f8bd86c3d71221c081eed1fed127ea2cc201a0
bfs07 <bfsalmeron@outlook.com>
2020-08-03 10:48:39 -0300

    Update persistent seg tree

--- codes/data_structures/persistent_segment_tree.cpp
+++ codes/data_structures/persistent_segment_tree.cpp
@@ -2,106 +2,89 @@ class Persistent_Seg_Tree {
   struct Node {
     int val;
     Node *left, *right;
-    Node() {}
-    Node(int v, Node *l, Node *r) : val(v), left(l), right(r) {}
+    Node(const int v) : val(v), left(nullptr), right(nullptr) {}
   };
-#define NEUTRAL_NODE Node(0, nullptr, nullptr);
-  Node _NEUTRAL_NODE = Node(0, nullptr, nullptr);
 
-public:
+private:
+  const Node NEUTRAL_NODE = Node(0);
   int merge_nodes(const int x, const int y) { return x + y; }
 
 private:
-  int n;
-  vector<Node *> version;
+  const int n;
+  vector<Node *> version = {nullptr};
 
 public:
-  Persistent_Seg_Tree() { this->n = -1; }
   /// Builds version[0] with the values in the array.
   ///
   /// Time complexity: O(n)
-  Node *pst_build(Node *node, const int l, const int r,
-                  const vector<int> &arr) {
-    node = new NEUTRAL_NODE;
+  Node *build(Node *node, const int l, const int r, const vector<int> &arr) {
+    node = new Node(NEUTRAL_NODE);
     if (l == r) {
       node->val = arr[l];
       return node;
     }
 
-    int mid = (l + r) / 2;
-    node->left = pst_build(node->left, l, mid, arr);
-    node->right = pst_build(node->right, mid + 1, r, arr);
+    const int mid = (l + r) / 2;
+    node->left = build(node->left, l, mid, arr);
+    node->right = build(node->right, mid + 1, r, arr);
     node->val = merge_nodes(node->left->val, node->right->val);
     return node;
   }
 
-  /// Builds version[0] with 0.
-  ///
-  /// Time complexity: O(n)
-  Node *pst_build_empty(Node *node, const int l, const int r) {
-    node = new NEUTRAL_NODE;
-    if (l == r)
-      return node;
+  Node *_update(Node *cur_tree, Node *prev_tree, const int l, const int r,
+                const int idx, const int delta) {
+    if (l > idx || r < idx)
+      return cur_tree != nullptr ? cur_tree : prev_tree;
 
-    int mid = (l + r) / 2;
-    node->left = pst_build_empty(node->left, l, mid);
-    node->right = pst_build_empty(node->right, mid + 1, r);
-    node->val = merge_nodes(node->left->val, node->right->val);
-    return node;
-  }
-
-  Node *pst_update(Node *cur_tree, Node *prev_tree, const int l, const int r,
-                   const int idx, const int delta) {
-    if (l > idx || r < idx) {
-      if (cur_tree != nullptr)
-        return cur_tree;
-      return prev_tree;
-    }
-
-    if (cur_tree == nullptr)
-      cur_tree = new Node(prev_tree->val, prev_tree->left, prev_tree->right);
-    else
-      cur_tree = new Node(cur_tree->val, cur_tree->left, cur_tree->right);
+    cur_tree = new Node(cur_tree == nullptr ? *prev_tree : *cur_tree);
 
     if (l == r) {
       cur_tree->val += delta;
       return cur_tree;
     }
 
-    int mid = (l + r) / 2;
+    const int mid = (l + r) / 2;
     cur_tree->left =
-        pst_update(cur_tree->left, prev_tree->left, l, mid, idx, delta);
+        _update(cur_tree->left, prev_tree->left, l, mid, idx, delta);
     cur_tree->right =
-        pst_update(cur_tree->right, prev_tree->right, mid + 1, r, idx, delta);
+        _update(cur_tree->right, prev_tree->right, mid + 1, r, idx, delta);
     cur_tree->val = merge_nodes(cur_tree->left->val, cur_tree->right->val);
     return cur_tree;
   }
 
-  int pst_query(Node *node, const int l, const int r, const int i,
-                const int j) {
+  int _query(Node *node, const int l, const int r, const int i, const int j) {
     if (l > j || r < i)
-      return _NEUTRAL_NODE.val;
+      return NEUTRAL_NODE.val;
 
     if (i <= l && r <= j)
       return node->val;
 
     int mid = (l + r) / 2;
-    return merge_nodes(pst_query(node->left, l, mid, i, j),
-                       pst_query(node->right, mid + 1, r, i, j));
+    return merge_nodes(_query(node->left, l, mid, i, j),
+                       _query(node->right, mid + 1, r, i, j));
+  }
+
+  void create_version(const int v) {
+    if (v >= this->version.size())
+      version.resize(v + 1);
   }
 
 public:
-  Persistent_Seg_Tree(const int n, const int number_of_versions) {
-    this->n = n;
-    version.resize(number_of_versions);
-    this->version[0] = this->pst_build_empty(this->version[0], 0, this->n - 1);
+  Persistent_Seg_Tree() : n(-1) {}
+
+  /// Constructor that initializes the segment tree empty.
+  ///
+  /// Time Complexity: O(n)
+  Persistent_Seg_Tree(const int n) : n(n) {
+    this->version[0] =
+        this->build(this->version[0], 0, this->n - 1, vector<int>(n, 0));
   }
 
   /// Constructor that allows to pass initial values to the leafs.
-  Persistent_Seg_Tree(const vector<int> &arr, const int number_of_versions) {
-    this->n = arr.size();
-    version.resize(number_of_versions);
-    this->version[0] = this->pst_build(this->version[0], 0, this->n - 1, arr);
+  ///
+  /// Time Complexity: O(n)
+  Persistent_Seg_Tree(const vector<int> &arr) : n(arr.size()) {
+    this->version[0] = this->build(this->version[0], 0, this->n - 1, arr);
   }
 
   /// Links the root of a version to a previous version.
@@ -109,9 +92,8 @@ public:
   /// Time Complexity: O(1)
   void link(const int version, const int prev_version) {
     assert(this->n > -1);
-    assert(0 <= prev_version);
-    assert(prev_version <= version);
-    assert(version < this->version.size());
+    assert(0 <= prev_version), assert(prev_version <= version),
+        assert(version < this->version.size());
     this->version[version] = this->version[prev_version];
   }
 
@@ -121,12 +103,11 @@ public:
   void update(const int cur_version, const int prev_version, const int idx,
               const int delta) {
     assert(this->n > -1);
-    assert(0 <= prev_version);
-    assert(prev_version <= cur_version);
-    assert(cur_version < this->version.size());
-    this->version[cur_version] = this->pst_update(this->version[cur_version],
-                                                  this->version[prev_version],
-                                                  0, this->n - 1, idx, delta);
+    assert(0 <= prev_version), assert(prev_version <= cur_version);
+    this->create_version(cur_version);
+    this->version[cur_version] =
+        this->_update(this->version[cur_version], this->version[prev_version],
+                      0, this->n - 1, idx, delta);
   }
 
   /// Query from l to r.
@@ -135,9 +116,7 @@ public:
   int query(const int version, const int l, const int r) {
     assert(this->n > -1);
     assert(this->version[version] != nullptr);
-    assert(0 <= l);
-    assert(l <= r);
-    assert(r < this->n);
-    return this->pst_query(this->version[version], 0, this->n - 1, l, r);
+    assert(0 <= l), assert(l <= r), assert(r < this->n);
+    return this->_query(this->version[version], 0, this->n - 1, l, r);
   }
 };
