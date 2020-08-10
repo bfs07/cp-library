fc46c41fd51b01c115eb343c6e25e8f8662a092e
bfs07 <bfsalmeron@outlook.com>
2020-08-10 17:25:39 -0300

    Update matrix expo to allow creation of dynamic and static matrices

--- codes/math/matrix_exponentiation_.cpp
+++ codes/math/matrix_exponentiation_.cpp
@@ -1,19 +1,36 @@
 struct Matrix {
-  static constexpr int N = 2, M = 2;
   static constexpr int MOD = 1e9 + 7;
-  array<array<int, M>, N> mat = {};
 
+  // // static matrix, if it's created multiple times, it's recommended
+  // // to avoid TLE.
+  // static constexpr int N = 2, M = 2;
+  // array<array<int, M>, N> mat = {};
+  // Matrix(const int n, const int m) {}
+
+  // // dynamic matrix
+  // int N, M;
+  // vector<vector<int>> mat;
+  // Matrix(const int n, const int m) : N(n), M(m) {
+  //   mat.resize(n, vector<int>(m));
+  // }
+
+  /// Creates a n x n identity matrix.
+  ///
+  /// Time Complexity: O(n*n)
   Matrix identity() {
     assert(N == M);
-    Matrix mat_identity;
+    Matrix mat_identity(N, M);
     for (int i = 0; i < N; ++i)
       mat_identity.mat[i][i] = 1;
     return mat_identity;
   }
 
+  /// Multiplies matrices mat and other.
+  ///
+  /// Time Complexity: O(mat.size() ^ 3)
   Matrix operator*(const Matrix &other) const {
     assert(mat.front().size() == other.mat.size());
-    Matrix ans;
+    Matrix ans(N, M);
     for (int i = 0; i < N; ++i)
       for (int j = 0; j < M; ++j)
         for (int k = 0; k < M; ++k)
@@ -21,10 +38,12 @@ struct Matrix {
     return ans;
   }
 
+  /// Exponentiates the matrix mat to the power of p.
+  ///
+  /// Time Complexity: O((mat.size() ^ 3) * log2(p))
   Matrix expo(int p) {
     assert(p >= 0);
-    Matrix ans = identity();
-    Matrix cur_power;
+    Matrix ans = identity(), cur_power(N, M);
     cur_power.mat = mat;
     while (p) {
       if (p & 1)
