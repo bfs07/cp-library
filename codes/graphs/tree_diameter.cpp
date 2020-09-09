e8cec95b001b362a9321889dd01042698aa1fd62
bfs07 <bfsalmeron@outlook.com>
2020-09-09 19:32:11 -0300

    Add tree diameter and update pdf

---
+++ codes/graphs/tree_diameter.cpp
@@ -0,0 +1,34 @@
+namespace tree {
+/// Returns a pair which contains the most distant vertex from src and the
+/// distance.
+pair<int, int> bfs(const int src, const vector<vector<int>> &adj) {
+  queue<tuple<int, int, int>> q;
+  q.emplace(0, src, -1);
+  int furthest = src, dist = 0;
+  while (!q.empty()) {
+    int d, u, p;
+    tie(d, u, p) = q.front();
+    q.pop();
+    if (d > dist) {
+      furthest = u;
+      dist = d;
+    }
+    for (int v : adj[u]) {
+      if (v == p)
+        continue;
+      q.emplace(d + 1, v, u);
+    }
+  }
+  return make_pair(furthest, dist);
+}
+
+/// Returns the length of the diameter and two vertices that belong to it.
+///
+/// Time Complexity: O(n)
+tuple<int, int, int> diameter(const int root_idx,
+                              const vector<vector<int>> &adj) {
+  int ini = bfs(root_idx, adj).first, end, dist;
+  tie(end, dist) = bfs(ini, adj);
+  return {dist, ini, end};
+}
+}; // namespace tree
