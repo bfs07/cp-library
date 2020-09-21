b396fbba4df8e7d05698dc60a124821eeb5172e8
bfs07 <bfsalmeron@outlook.com>
2020-09-20 23:02:38 -0300

    Update dsu

--- codes/graphs/dsu.cpp
+++ codes/graphs/dsu.cpp
@@ -1,34 +1,36 @@
 class DSU {
+  vector<int> root, sz;
+
 public:
-  vector<int> root;
-  vector<int> sz;
-
-  DSU(int n) {
-    this->root.resize(n + 1);
-    iota(this->root.begin(), this->root.begin() + n + 1, 0ll);
-    this->sz.resize(n + 1, 1);
+  DSU(const int n) {
+    root.resize(n + 1);
+    iota(root.begin(), root.begin() + n + 1, 0ll);
+    sz.resize(n + 1, 1);
   }
 
-  int Find(int x) {
-    if (this->root[x] == x)
+  /// Returns the id of the set in which the element x belongs.
+  ///
+  /// Time Complexity: O(1)
+  int Find(const int x) {
+    if (root[x] == x)
       return x;
-    return this->root[x] = this->Find(this->root[x]);
+    return root[x] = Find(root[x]);
   }
 
+  /// Unites two sets in which p and q belong.
+  /// Returns false if they already belong to the same set.
+  ///
+  /// Time Complexity: O(1)
   bool Union(int p, int q) {
-    p = this->Find(p), q = this->Find(q);
-
+    p = Find(p), q = Find(q);
     if (p == q)
       return false;
 
-    if (this->sz[p] > this->sz[q]) {
-      this->root[q] = p;
-      this->sz[p] += this->sz[q];
-    } else {
-      this->root[p] = q;
-      this->sz[q] += this->sz[p];
-    }
+    if (sz[p] < sz[q])
+      swap(p, q);
 
+    root[q] = p;
+    sz[p] += sz[q];
     return true;
   }
 };
