3b3029cb1c4868e90a8180e8cc10281abe77263c
bfs07 <bfsalmeron@outlook.com>
2020-09-20 23:00:13 -0300

    Update bit

--- codes/data_structures/bit.cpp
+++ codes/data_structures/bit.cpp
@@ -5,63 +5,75 @@ private:
   int n;
 
 private:
-  int low(const int i) { return (i & (-i)); }
+  int low(const int i) { return i & (-i); }
 
-  // point update
+  // Point update
   void bit_update(int i, const int delta) {
-    while (i <= this->n) {
-      this->bit[i] += delta;
-      i += this->low(i);
+    while (i <= n) {
+      bit[i] += delta;
+      i += low(i);
     }
   }
 
-  // point query
+  // Prefix query
   int bit_query(int i) {
     int sum = 0;
     while (i > 0) {
       sum += bit[i];
-      i -= this->low(i);
+      i -= low(i);
     }
     return sum;
   }
 
-public:
-  BIT(const vector<int> &arr) { this->build(arr); }
-
-  BIT(const int n) {
-    // OBS: BIT IS INDEXED FROM 1
-    // THE USE OF 1-BASED ARRAY IS RECOMMENDED
-    this->n = n;
-    this->bit.resize(n + 1, 0);
-  }
-
-  // build the bit
+  // Builds the bit
   void build(const vector<int> &arr) {
     // OBS: BIT IS INDEXED FROM 1
-    // THE USE OF 1-BASED ARRAY IS RECOMMENDED
+    // THE USAGE OF 1-BASED ARRAY IS MANDATORY
     assert(arr.front() == 0);
     this->n = (int)arr.size() - 1;
-    this->bit.resize(arr.size(), 0);
+    bit.resize(arr.size(), 0);
 
-    for (int i = 1; i <= this->n; i++)
-      this->bit_update(i, arr[i]);
+    for (int i = 1; i <= n; i++)
+      bit_update(i, arr[i]);
   }
 
-  // point update
+public:
+  /// Constructor responsible for initializing the tree with 0's.
+  ///
+  /// Time Complexity: O(n log n)
+  BIT(const vector<int> &arr) { build(arr); }
+
+  /// Constructor responsible for building the tree based on a vector.
+  ///
+  /// Time Complexity O(n)
+  BIT(const int n) {
+    // OBS: BIT IS INDEXED FROM 1
+    // THE USAGE OF 1-BASED ARRAY IS MANDATORY
+    this->n = n;
+    bit.resize(n + 1, 0);
+  }
+
+  /// Update at a single index.
+  ///
+  /// Time Complexity O(log n)
   void update(const int i, const int delta) {
-    assert(1 <= i), assert(i <= this->n);
-    this->bit_update(i, delta);
+    assert(1 <= i), assert(i <= n);
+    bit_update(i, delta);
   }
 
-  // point query
+  /// Prefix query from 1 to i.
+  ///
+  /// Time Complexity O(log n)
   int query(const int i) {
-    assert(1 <= i), assert(i <= this->n);
-    return this->bit_query(i);
+    assert(1 <= i), assert(i <= n);
+    return bit_query(i);
   }
 
-  // range query
+  /// Range query from l to r.
+  ///
+  /// Time Complexity O(log n)
   int query(const int l, const int r) {
-    assert(1 <= l), assert(l <= r), assert(r <= this->n);
-    return this->bit_query(r) - this->bit_query(l - 1);
+    assert(1 <= l), assert(l <= r), assert(r <= n);
+    return bit_query(r) - bit_query(l - 1);
   }
 };
