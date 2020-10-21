d4b6a3392e24b8c9c69aa8c294562656dd1db737
bfs07 <bfsalmeron@outlook.com>
2020-10-20 21:25:16 -0300

    Update 2-SAT

--- codes/miscellaneous/2-SAT.cpp
+++ codes/miscellaneous/2-SAT.cpp
@@ -1,40 +1,35 @@
-// REQUIRES SCC code
-
 // OBS: INDEXED FROM 0
+// USE POS_X = 1 FOR POSITIVE CLAUSES AND 0 FOR NEGATIVE. OTHERWISE THE FINAL
+// ANSWER ARRAY WILL BE FLIPPED.
 class SAT {
 private:
   vector<vector<int>> adj;
   int n;
 
 public:
-  // Contains the value for an possible answer. Take care, depending on the
-  // implementation the value of the i-th variable may be negative or positive.
-  vector<bool> ans;
-
-  SAT(int n) {
-    this->n = n;
+  SAT(const int n) : n(n) {
     adj.resize(2 * n);
     ans.resize(n);
   }
 
   //  (X v Y)  = (~X -> Y) & (~Y -> X)
-  void add_or(int x, bool pos_x, int y, bool pos_y) {
-    assert(0 <= x), assert(x < n);
-    assert(0 <= y), assert(y < n);
+  void add_or(const int x, const bool pos_x, const int y, const bool pos_y) {
+    assert(0 <= x), assert(x < n), assert(0 <= y), assert(y < n);
     adj[(x << 1) ^ (pos_x ^ 1)].emplace_back((y << 1) ^ pos_y);
     adj[(y << 1) ^ (pos_y ^ 1)].emplace_back((x << 1) ^ pos_x);
   }
 
   // (X xor Y) = (X v Y) & (~X v ~Y)
-  // for this function the result is always 0 1 or 1 0
-  void add_xor(int x, bool pos_x, int y, bool pos_y) {
-    assert(0 <= x), assert(x < n);
-    assert(0 <= y), assert(y < n);
-    add_or(x, y, pos_x, pos_y);
-    add_or(x, y, pos_x ^ 1, pos_y ^ 1);
+  // for this operation the result is always 0 1 or 1 0
+  void add_xor(const int x, const bool pos_x, const int y, const bool pos_y) {
+    assert(0 <= x), assert(x < n), assert(0 <= y), assert(y < n);
+    add_or(x, pos_x, y, pos_y);
+    add_or(x, pos_x ^ 1, y, pos_y ^ 1);
   }
 
-  /// Checks whether the system is feasible or not.
+  vector<bool> ans;
+  /// Checks whether the system is feasible or not. If it's feasible, it stores
+  /// a satisfable answer in the array `ans`.
   ///
   /// Time Complexity: O(n)
   bool check() {
@@ -42,7 +37,7 @@ public:
     for (int i = 0; i < n; i++) {
       if (scc.comp[(i << 1) | 1] == scc.comp[(i << 1) | 0])
         return false;
-      ans[i] = (scc.comp[(i << 1) | 1] < scc.comp[(i << 1) | 0]);
+      ans[i] = (scc.comp[(i << 1) | 1] > scc.comp[(i << 1) | 0]);
     }
     return true;
   }
