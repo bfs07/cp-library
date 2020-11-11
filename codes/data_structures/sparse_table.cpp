a78890363e8ae8480a4bf35d6cc5d07cdb69017d
bfs07 <bfsalmeron@outlook.com>
2020-11-11 16:45:41 -0300

    Format and Coment type

--- codes/data_structures/sparse_table.cpp
+++ codes/data_structures/sparse_table.cpp
@@ -1,7 +1,7 @@
 class Sparse_Table {
 private:
   /// Sparse table min
-  int merge(const int l, const int r) { return min(l, r); }
+  // int merge(const int l, const int r) { return min(l, r); }
   /// Sparse table max
   // int merge(const int l, const int r) { return max(l, r); }
 
@@ -25,10 +25,9 @@ private:
     table[0] = arr;
     int pow2 = 1;
     for (int i = 1; i < table.size(); i++) {
-      int lastsz = this->n - pow2 + 1;
-      for (int j = 0; j + pow2 < lastsz; j++) {
+      const int lastsz = this->n - pow2 + 1;
+      for (int j = 0; j + pow2 < lastsz; j++)
         table[i][j] = merge(table[i - 1][j], table[i - 1][j + pow2]);
-      }
       pow2 <<= 1;
     }
   }
@@ -45,10 +44,9 @@ public:
   void print() {
     int pow2 = 1;
     for (int i = 0; i < table.size(); i++) {
-      int sz = (int)(table.front().size()) - pow2 + 1;
-      for (int j = 0; j < sz; j++) {
+      const int sz = (int)(table.front().size()) - pow2 + 1;
+      for (int j = 0; j < sz; j++)
         cout << table[i][j] << " \n"[(j + 1) == sz];
-      }
       pow2 <<= 1;
     }
   }
@@ -57,9 +55,7 @@ public:
   ///
   /// Time Complexity: O(1)
   int query(const int l, const int r) {
-    assert(l <= r);
-    assert(0 <= l && r <= this->n - 1);
-
+    assert(0 <= l), assert(l <= r), assert(r < this->n);
     int lgg = lg[r - l + 1];
     return merge(table[lgg][l], table[lgg][r - (1 << lgg) + 1]);
   }
