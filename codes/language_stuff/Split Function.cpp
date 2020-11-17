a129f94ca0574c6e07b91f09b66f30ab8f8c3462
bfs07 <bfsalmeron@outlook.com>
2020-11-17 19:40:17 -0300

    Format split funcion

--- codes/language_stuff/Split Function.cpp
+++ codes/language_stuff/Split Function.cpp
@@ -1,15 +1,14 @@
-// SEPARA STRING POR UM DELIMITADOR
-// EX: str=A-B-C split -> x = {A,B,C} 
-vector<string> split(const string &s, char delim) {
-    stringstream ss(s);
-    string item;
-    vector<string> tokens;
-    while (getline(ss, item, delim)) {
-        tokens.push_back(item);
-    }
-    return tokens;
+/// Splits a string into a vector. A separator can be specified
+/// EX: str=A-B-C -> split -> x = {A,B,C}
+vector<string> split(const string &s, char separator = ' ') {
+  stringstream ss(s);
+  string item;
+  vector<string> tokens;
+  while (getline(ss, item, separator))
+    tokens.emplace_back(item);
+  return tokens;
 }
-int main () {
-  vector<string> x = split("cap-one-best-opinion-language", '-'); 
-// x = {cap,one,best,opinion,language};
+int main() {
+  vector<string> x = split("cap-one-best-opinion-language", '-');
+  // x = {cap,one,best,opinion,language};
 }
