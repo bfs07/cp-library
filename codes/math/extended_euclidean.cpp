24defe66c42a86f7b93f458fe94532f084eff3c2
bfs07 <bfsalmeron@outlook.com>
2020-11-17 19:40:53 -0300

    Update extended_gcd to thalles code

--- codes/math/extended_euclidean.cpp
+++ codes/math/extended_euclidean.cpp
@@ -1,16 +1,14 @@
-int gcd, x, y;
-
-// Ax + By = gcd(A,B)
-
-void extended_euclidian(const int a, const int b) {
-  if (b == 0) {
-    gcd = a;
-    x = 1;
-    y = 0;
-  } else {
-    extended_euclidian(b, a % b);
-    const int temp = x;
-    x = y;
-    y = temp - (a / b) * y;
+/// Returns a tuple containing the gcd(a, b) and the roots for
+/// a*x + b*y = gcd(a, b).
+///
+/// Time Complexity: O(log(min(a, b))).
+tuple<uint, int, int> extended_gcd(uint a, uint b) {
+  int x = 0, y = 1, x1 = 1, y1 = 0;
+  while (a != 0) {
+    uint q = b / a;
+    tie(x, x1) = make_pair(x1, x - q * x1);
+    tie(y, y1) = make_pair(y1, y - q * y1);
+    tie(a, b) = make_pair(b % a, a);
   }
+  return make_tuple(b, x, y);
 }
