1bed945f7865f72b5879b95583d61d5ff5d0e85a
bfs07 <bfsalmeron@outlook.com>
2020-12-21 11:50:15 -0300

    Add rollback to dsu

--- codes/graphs/dsu.cpp
+++ codes/graphs/dsu.cpp
@@ -1,7 +1,9 @@
+// Remove comments to add rollback
 class DSU {
-  vector<int> root, sz;
-
 public:
+  vector<int> root, sz;
+  // stack<tuple<int, int, int>> old_root, old_sz;
+
   DSU(const int n) {
     root.resize(n + 1);
     iota(root.begin(), root.begin() + n + 1, 0ll);
@@ -15,22 +17,36 @@ public:
     if (root[x] == x)
       return x;
     return root[x] = Find(root[x]);
+    // DONT USE PATH COMPRESSION WITH ROLLBACK!!
+    // return Find(root[x]);
   }
 
-  /// Unites two sets in which p and q belong.
+  /// Unites two sets in which u and v belong.
   /// Returns false if they already belong to the same set.
   ///
   /// Time Complexity: O(1)
-  bool Union(int p, int q) {
-    p = Find(p), q = Find(q);
-    if (p == q)
+  bool Union(int u, int v /* , int idx */) {
+    u = Find(u), v = Find(v);
+    if (u == v)
       return false;
 
-    if (sz[p] < sz[q])
-      swap(p, q);
+    if (sz[u] < sz[v])
+      swap(u, v);
 
-    root[q] = p;
-    sz[p] += sz[q];
+    // old_root.emplace(idx, v, root[v]);
+    // old_sz.emplace(idx, u, sz[u]);
+    root[v] = u;
+    sz[u] += sz[v];
     return true;
   }
+
+  // void rollback() {
+  //   int idx, u, val;
+  //   tie(idx, u, val) = old_root.top();
+  //   old_root.pop();
+  //   root[u] = val;
+  //   tie(idx, u, val) = old_sz.top();
+  //   old_sz.pop();
+  //   sz[u] = val;
+  // }
 };
