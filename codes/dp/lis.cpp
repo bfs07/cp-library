697dd302f8047e1d721af54fb60be72a924c1a36
bfs07 <bfsalmeron@outlook.com>
2021-01-03 17:21:30 -0300

    Update lis

--- codes/dp/lis.cpp
+++ codes/dp/lis.cpp
@@ -3,21 +3,21 @@ int lis(vector<int> &arr) {
   vector<int> lis;
   for (int i = 0; i < n; i++) {
     int l = 0, r = (int)lis.size() - 1;
-    int ansj = -1;
+    int ans = -1;
     while (l <= r) {
       int mid = (l + r) / 2;
       // OBS: - To >= LIS change to the operation below to >
       //      - Put <= or >= for strictly!!
       if (arr[i] < lis[mid]) {
         r = mid - 1;
-        ansj = mid;
+        ans = mid;
       } else
         l = mid + 1;
     }
-    if (ansj == -1)
+    if (ans == -1)
       lis.emplace_back(arr[i]);
     else
-      lis[ansj] = arr[i];
+      lis[ans] = arr[i];
   }
 
   return lis.size();
