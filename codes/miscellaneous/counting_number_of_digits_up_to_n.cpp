e88a2f311947b4873a3fad3e93da033487785ce0
bfs07 <bfsalmeron@outlook.com>
2021-01-03 16:27:20 -0300

    Update and rename digits count

--- codes/miscellaneous/counting_number_of_digits_up_to_n.cpp
+++ codes/miscellaneous/counting_number_of_digits_up_to_n.cpp
@@ -1,12 +1,9 @@
 int solve(int n) {
-
-	int maxx = 9, minn = 1, dig = 1, ret = 0;
-
-	for(int i = 1; i <= 17; i++) {
-		int q = min(maxx, n);
-		ret += max(0ll, (q - minn + 1) * dig);
-		maxx = (maxx * 10 + 9), minn *= 10, dig++;
-	}
-
-	return ret;
+  int maxx = 9, minn = 1, dig = 1, ret = 0;
+  for (int i = 1; i <= 17; i++) {
+    int q = min(maxx, n);
+    ret += max(0ll, (q - minn + 1) * dig);
+    maxx = (maxx * 10 + 9), minn *= 10, dig++;
+  }
+  return ret;
 }
