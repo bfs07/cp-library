3257d1096f73fef80e17a755ee1aa491c1c00b98
bfs07 <bfsalmeron@outlook.com>
2021-01-09 17:22:16 -0300

    Add support to indexed bit and range update

--- codes/data_structures/bit.cpp
+++ codes/data_structures/bit.cpp
@@ -1,14 +1,17 @@
-/// INDEX THE ARRAY BY 1!!!
+// #define RANGE_SUM
+// #define RANGE_UPDATE
+// Uncomment ONLY ONE above!
+// clang-format off
 class BIT {
 private:
   vector<int> bit;
-  int n;
+  const int n, offset;
 
 private:
   int low(const int i) { return i & (-i); }
 
   // Point update
-  void bit_update(int i, const int delta) {
+  void _update(int i, const int delta) {
     while (i <= n) {
       bit[i] += delta;
       i += low(i);
@@ -16,7 +19,7 @@ private:
   }
 
   // Prefix query
-  int bit_query(int i) {
+  int _query(int i) {
     int sum = 0;
     while (i > 0) {
       sum += bit[i];
@@ -25,63 +28,74 @@ private:
     return sum;
   }
 
-  // Builds the bit
   void build(const vector<int> &arr) {
-    // OBS: BIT IS INDEXED FROM 1
-    // THE USAGE OF 1-BASED ARRAY IS MANDATORY
-    assert(arr.front() == 0);
-    this->n = (int)arr.size() - 1;
-    bit.resize(arr.size(), 0);
-
+    bit.resize(arr.size() + offset, 0);
     for (int i = 1; i <= n; i++)
-      bit_update(i, arr[i]);
+      #ifdef RANGE_UPDATE
+      update(i - offset, i - offset, arr[i - offset]);
+      #endif
+      #ifdef RANGE_SUM
+      update(i - offset, arr[i - offset]);
+      #endif
   }
 
 public:
   /// Constructor responsible for initializing the tree with 0's.
   ///
   /// Time Complexity: O(n log n)
-  BIT(const vector<int> &arr) { build(arr); }
+  BIT(const vector<int> &arr, const int indexed_from)
+      : n(arr.size() - indexed_from), offset(indexed_from ^ 1) {
+    assert(indexed_from == 0 || indexed_from == 1);
+    build(arr);
+  }
 
   /// Constructor responsible for building the tree based on a vector.
   ///
   /// Time Complexity O(n)
-  BIT(const int n) {
-    // OBS: BIT IS INDEXED FROM 1
-    // THE USAGE OF 1-BASED ARRAY IS MANDATORY
-    this->n = n;
+  BIT(const int n, const int indexed_from) : n(n), offset(indexed_from ^ 1) {
     bit.resize(n + 1, 0);
   }
 
+  #ifdef RANGE_UPDATE
+  void update(int l, int r, const int val) {
+    l += offset, r += offset;
+    assert(1 <= l), assert(l <= r), assert(r <= n);
+    _update(l, val);
+    _update(r + 1, -val);
+  }
+  #endif
+
+  #ifdef RANGE_SUM
   /// Update at a single index.
   ///
   /// Time Complexity O(log n)
-  void update(const int i, const int delta) {
-    assert(1 <= i), assert(i <= n);
-    bit_update(i, delta);
+  void update(int idx, const int delta) {
+    idx += offset;
+    assert(1 <= idx), assert(idx <= n);
+    _update(idx, delta);
   }
+  #endif
 
-  /// Prefix query from 1 to i.
-  ///
-  /// Time Complexity O(log n)
-  int prefix_query(const int i) {
-    assert(1 <= i), assert(i <= n);
-    return bit_query(i);
-  }
-
+  #ifdef RANGE_UPDATE
   /// Query at a single index.
   ///
   /// Time Complexity O(log n)
-  int query(const int idx) {
-    assert(1 <= idx), assert(idx <= this->n);
-    return bit_query(idx) - bit_query(idx - 1);
+  int query(int idx) {
+    idx += offset;
+    assert(1 <= idx), assert(idx <= n);
+    return _query(idx);
   }
+  #endif
 
+  #ifdef RANGE_SUM
   /// Range query from l to r.
   ///
   /// Time Complexity O(log n)
-  int query(const int l, const int r) {
+  int query(int l, int r) {
+    l += offset, r += offset;
     assert(1 <= l), assert(l <= r), assert(r <= n);
-    return bit_query(r) - bit_query(l - 1);
+    return _query(r) - _query(l - 1);
   }
+  #endif
 };
+// clang-format on
