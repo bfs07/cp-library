623b89bdc5724270b14f8e48ddc6f03cda823f0e
bfs07 <bfsalmeron@outlook.com>
2021-01-14 11:05:45 -0300

    Rename and update catalan code

---
+++ codes/dp/catalan_1_1_2_5_14_42_132.cpp
@@ -0,0 +1,16 @@
+// The first few Catalan numbers for n = 0, 1, 2, 3, ...
+// are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, ...
+// Formula Recursiva:
+// cat(0) = 0
+// cat(n+1) = sum(i from 0 to n)(cat(i)*cat(n-i))
+//
+// Using Binomial Coefficient
+// We can also use the below formula to find nth catalan number in O(n) time.
+
+// Returns value of Binomial Coefficient C(n, k)
+
+// REQUIRES combinatorics.cpp
+int catalan(int n) {
+  return comb.fat(2 * n) * comb.inv(comb.fat(n + 1)) % MOD *
+         comb.inv(comb.fat(n)) % MOD;
+}
