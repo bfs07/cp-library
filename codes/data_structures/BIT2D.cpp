af35fab0fb6e3d2b947f0e502e8d4947f672cd96
bfs07 <bfs.07@hotmail.com>
2021-03-13 17:14:59 -0300

    Add bit2D

--- codes/data_structures/BIT2D.cpp
+++ codes/data_structures/BIT2D.cpp
@@ -1,81 +1,71 @@
 // INDEX BY ONE ALWAYS!!!
 class BIT_2D {
- private:
+private:
   // row, column
-  int n, m;
+  const int n, m;
   vector<vector<int>> tree;
 
- private:
+private:
   // Returns an integer which constains only the least significant bit.
-  int low(int i) {
-    return i & (-i);
+  int low(const int i) { return i & (-i); }
+
+  void _update(const int x, const int y, const int delta) {
+    for (int i = x; i < n; i += low(i))
+      for (int j = y; j < m; j += low(j))
+        tree[i][j] += delta;
   }
 
-  void bit_update(const int x, const int y, const int delta) {
-    for(int i = x; i < n; i += low(i))
-      for(int j = y; j < m; j += low(j))
-        this->tree[i][j] += delta;
-  }
-
-  int bit_query(const int x, const int y) {
+  int _query(const int x, const int y) {
     int ans = 0;
-    for(int i = x; i > 0; i -= low(i))
-      for(int j = y; j > 0; j -= low(j))
-        ans += this->tree[i][j];
-
+    for (int i = x; i > 0; i -= low(i))
+      for (int j = y; j > 0; j -= low(j))
+        ans += tree[i][j];
     return ans;
   }
 
- public:
+public:
   // put the size of the array without 1 indexing.
   /// Time Complexity: O(n * m)
-  BIT_2D(int n, int m) {
-    this->n = n + 1;
-    this->m = m + 1;
-
-    this->tree.resize(n, vector<int>(m, 0));
+  BIT_2D(const int n, const int m) : n(n + 1), m(m + 1) {
+    tree.resize(this->n, vector<int>(this->m, 0));
   }
 
   /// Time Complexity: O(n * m * (log(n) + log(m)))
-  BIT_2D(const vector<vector<int>> &mat) {
-    // Check if it is 1 index.
+  BIT_2D(const vector<vector<int>> &mat)
+      : n(mat.size()), m(mat.front().size()) {
+    // Check if it is 1 indexed.
     assert(mat[0][0] == 0);
-    this->n = mat.size();
-    this->m = mat.front().size();
-    
-    this->tree.resize(n, vector<int>(m, 0));
-    for(int i = 1; i < n; i++)
-      for(int j = 1; j < m; j++)
-        update(i, j, mat[i][j]);
+    tree.resize(n, vector<int>(m, 0));
+    for (int i = 1; i < n; i++)
+      for (int j = 1; j < m; j++)
+        _update(i, j, mat[i][j]);
   }
 
   /// Query from (1, 1) to (x, y).
   ///
   /// Time Complexity: O(log(n) + log(m))
   int prefix_query(const int x, const int y) {
-    assert(0 < x); assert(x < this->n);
-    assert(0 < y); assert(y < this->m);
-
-    return bit_query(x, y);
+    assert(0 < x), assert(x < n);
+    assert(0 < y), assert(y < m);
+    return _query(x, y);
   }
 
   /// Query from (x1, y1) to (x2, y2).
   ///
   /// Time Complexity: O(log(n) + log(m))
   int query(const int x1, const int y1, const int x2, const int y2) {
-    assert(0 < x1); assert(x1 <= x2); assert(x2 < this->n);
-    assert(0 < y1); assert(y1 <= y2); assert(y2 < this->m);
-
-    return bit_query(x2, y2) - bit_query(x1 - 1, y2) - bit_query(x2, y1 - 1) + bit_query(x1 - 1, y1 - 1);
+    assert(0 < x1), assert(x1 <= x2), assert(x2 < n);
+    assert(0 < y1), assert(y1 <= y2), assert(y2 < m);
+    return _query(x2, y2) - _query(x1 - 1, y2) - _query(x2, y1 - 1) +
+           _query(x1 - 1, y1 - 1);
   }
 
   /// Updates point (x, y).
   ///
   /// Time Complexity: O(log(n) + log(m))
   void update(const int x, const int y, const int delta) {
-    assert(0 < x); assert(x < this->n);
-    assert(0 < y); assert(y < this->m);
-
-    bit_update(x, y, delta);  
+    assert(0 < x), assert(x < n);
+    assert(0 < y), assert(y < m);
+    _update(x, y, delta);
   }
 };
