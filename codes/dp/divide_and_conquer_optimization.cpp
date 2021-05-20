a357ee9e9131a42eb9e6d105b065ceb2dee36e20
bfs07 <bfs.07@hotmail.com>
2021-05-20 20:28:11 -0300

    Update d&c optimization

--- codes/dp/divide_and_conquer_optimization.cpp
+++ codes/dp/divide_and_conquer_optimization.cpp
@@ -2,16 +2,11 @@
 /// the square sum of each subarray.
 
 // resize below
-vi last, dp;
-// Cost for splitting the subarray (l, n - 1) into (l, r - 1), (r, n - 1).
+vector<int> last(MAXN, INF), dp(MAXN, INF);
+// Cost for the subarray (l, r).
 int C(int l, int r) {
   int val = 0;
-  // remove the cost for the range(l, n - 1)
-  val -= sq(sum(l, n - 1));
-  // adding the cost for the range(l, r - 1)
-  val += sq(sum(l, r - 1));
-  // adding the cost for the range(r, n - 1)
-  val += sq(sum(r, n - 1));
+  val += sq(sum(l, r));
   return val;
 }
 
@@ -26,12 +21,22 @@ void f(int l, int r, int optl, int optr) {
   int mid = (l + r) / 2;
   auto best = make_pair(INF, INF); // change to (-INF, -INF) to maximize
   // change mid - 1 to mid if buckets can intercept.
-  for (int i = optl; i <= min(mid - 1, optr); ++i)
-    best = min(best, {last[i] + C(i, mid), i});
+  for (int i = optl; i <= min(mid, optr); ++i)
+    best = min(best, {(i == 1 ? 0 : last[i - 1]) + C(i, mid), i});
 
   dp[mid] = best.first;
-  int opt = best.second;
+  const int opt = best.second;
 
   f(l, mid - 1, optl, opt);
   f(mid + 1, r, opt, optr);
+}
+
+// 1-indexed, change to 0-index if necessary.
+int compute(const int k) {
+  for (int i = 0; i < k; ++i) {
+    f(1, n, 1, n);
+    last.swap(dp);
+  }
+
+  return last[n];
 }
