4627bbcbc93cafac3de41f35cf0d08b3e3570d4e
bfs07 <bfs.07@hotmail.com>
2021-07-19 23:05:54 -0300

    Remove non-authoral and ugly code

--- codes/geometry/Distance Point Segment.cpp
+++
@@ -1,8 +0,0 @@
-// use struct point and line
-double dist_point_segment(const Point p, const Point s, const Point t) {
-    if(sgn(dot(p-s, t-s)) < 0) 
-      return (p-s).norm();
-    if(sgn(dot(p-t, s-t)) < 0) 
-      return (p-t).norm();
-    return abs(det(s-p, t-p) / dist(s, t));
-}
--- codes/geometry/Polygon Area.cpp
+++
@@ -1,9 +0,0 @@
-double polygonArea(vector<int> &X, vector<int> &Y, int n) {
-  int area = 0;
-  int j = n - 1;
-  for (int i = 0; i < n; i++) {
-    area += (X[j] + X[i]) * (Y[j] - Y[i]);
-    j = i;
-  }
-  return abs(area / 2.0);
-}
